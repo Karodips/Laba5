dd92692bcd2cc208b5bc9a5fe46342a938de6d47
Karodips <54810893+Karodips@users.noreply.github.com>
2019-09-17 15:11:59 +0300

    Update PerestanovkaLevogoChela.cpp

--- PerestanovkaLevogoChela.cpp
+++ PerestanovkaLevogoChela.cpp
@@ -7,8 +7,11 @@ int main() {
 	setlocale(LC_ALL, "Russian");
 	int a;
 	cin >> a;
+	if ( a > 99 && a < 1000) {
 	a = (a % 100 * 10) + (a / 100);
 	cout << a << endl;
+	}
+	else cout << "wait that's illegal!\n";
 	system("pause");
 	return 0;
 }
