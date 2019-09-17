c61fe1ccec975817b4191b2b5a7b11aa8462e16a
Karodips <54810893+Karodips@users.noreply.github.com>
2019-09-17 14:53:32 +0300

    Create Otrezki.cpp

---
+++ Otrezki.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+	setlocale(LC_ALL, "Russian");
+	int a, b, c;
+	cin >> a >> b;
+	if (a > b || a < 0 || b < 0) {
+		c = a / b;
+		a = a - b * c;
+		cout << a << "\n";
+	}
+	else cout << "wryyyy\n";
+	system("pause");
+	return 0;
+}
