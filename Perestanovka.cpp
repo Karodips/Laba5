515e1d0ed01beecbc99a0963ca1265dc2d37cbeb
Karodips <54810893+Karodips@users.noreply.github.com>
2019-09-17 15:10:42 +0300

    Update Perestanovka.cpp

--- Perestanovka.cpp
+++ Perestanovka.cpp
@@ -7,10 +7,13 @@ int main() {
 	setlocale(LC_ALL, "Russian");
 	int a, b;
 	cin >> a;
+	if ( a > 9 && a < 100) {
 	b = a / 10;
 	a = a - (b * 10);
 	a = a * 10 + b;
 	cout << a << endl;
+	}
+	else cout << "wait that's illegal!";
 	system("pause");
 	return 0;
 }
